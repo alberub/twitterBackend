ce60c41ca61f803781a5c5d01583be4f8761e0e1
Alberto Rios <rios.alb2606@gmail.com>
2021-07-19 18:18:16 -0500

    spa control

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 require( 'dotenv' ).config();
+
+const path = require('path');
 const cors = require('cors');
 const { dbConnection } = require('./database/config');
 
@@ -26,6 +28,10 @@ app.use( '/api/todo', require('./routes/busquedas') );
 app.use( '/api/login', require('./routes/auth') );
 
 app.use( '/api/upload', require('./routes/uploads') );
+
+app.get( '*', ( req, res ) => {
+    res.sendFile( path.resolve(__dirname, 'public/index.html') )
+});
 
 app.listen( process.env.PORT, () => {
     console.log('Servidor corriendo en puerto' + process.env.PORT);
